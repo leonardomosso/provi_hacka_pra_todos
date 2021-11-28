ed911cc27308eb34ef8422f4acc4fb32c2f787bf
Daniel <daniel.repres@hotmail.com>
2021-11-28 20:20:00 -0300

    Atualizado

---
+++ provi_hack/src/CardAbilities/CardAbilities.js
@@ -0,0 +1,29 @@
+import React from "react";
+import styled from "styled-components"
+
+
+const Card = styled.div`
+    display:flex;
+    width: 600px;
+    background-color: #EF6F7E;
+    border-radius: 20px;
+`
+
+const CardAbilities = (props) => {
+    console.log(props)
+    return (
+        <div>
+            <Card>
+                <div>
+                    <img src={props.img} alt="" />
+                </div>
+                <div>
+                    <h1>{props.titulo}</h1>
+                    <p>{props.texto}</p>
+                </div>
+            </Card>
+        </div>
+    )
+}
+
+export default CardAbilities;
